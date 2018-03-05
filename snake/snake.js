cca892f321df55ab6905c7f88b6b5dcc02fb9453
garyleefight <garyleefight@outlook.com>
2018-03-05 12:53:18 -0600

    add snake move

--- snake/snake.js
+++ snake/snake.js
@@ -3,18 +3,45 @@ var mapWidth = 900;
 var spanHeight = 45;
 var spanWidth = 45;
 var blankList = [];
-var snakeHead;
+var snakeList = [];
+var mapW = mapWidth / spanWidth;
+var mapH = mapHeight / spanHeight;
 var dir={
     dir_left: 1,
     dir_right: 2,
     dir_up: 3,
     dir_down: 4
 };
+var curDir = 4;
 
 window.onload = function(){
     var map = document.getElementById('map');
     initial(map);
     setInterval(logic, 400);
+    document.addEventListener('keydown', event => {
+        switch(event.keyCode){
+            case 37:
+                if(curDir != 1){
+                    curDir = dir.dir_left;
+                }
+                break;
+            case 39:
+                if(curDir != 2){
+                    curDir = dir.dir_right;
+                }
+                break;
+            case 38:
+                if(curDir != 3){
+                    curDir = dir.dir_up;
+                }
+                break;
+            case 40:
+                if(curDir != 4){
+                    curDir = dir.dir_down;
+                }
+                break;
+        }
+    })
 }
 
 function initial(map){
@@ -33,16 +60,44 @@ function initial(map){
 }
 
 function logic(){
-    if(blankList.length == 0){
+    if(snakeList.length == 0){
         temp = document.getElementById('1');
         temp.className = 'snake';
-        blankList.push(temp);
+        snakeList.push(temp);
     }else{
-        if(blankList.length <= 400){
-            temp = document.getElementById(blankList.length+1);
-            temp.className = 'snake';
-            blankList.push(temp);
+        snakeHead = parseInt(snakeList[snakeList.length - 1].id);
+        switch(curDir){
+            case 1:
+                if(snakeHead % mapW == 1){
+                    tempSpan = document.getElementById(snakeHead + mapW - 1);
+                }else{
+                tempSpan = document.getElementById(snakeHead - 1);
+                }
+                break;
+            case 2:
+                if(snakeHead % mapW == 0){
+                    tempSpan = document.getElementById(snakeHead - mapW + 1);
+                }else{
+                tempSpan = document.getElementById(snakeHead + 1);
+                }
+                break;
+            case 3:
+                if(snakeHead - mapW <= 0){
+                    tempSpan = document.getElementById(snakeHead + (mapH - 1)*mapW);
+                }else{
+                tempSpan = document.getElementById(snakeHead - mapH);
+                }
+                break;
+            case 4:
+                if(snakeHead + mapW > mapW*mapH){
+                    tempSpan = document.getElementById(snakeHead - (mapH - 1)*mapW);
+                }else{
+                tempSpan = document.getElementById(snakeHead + mapH);
+                }
         }
+        tempSpan.className = 'snake';
+                snakeList.push(tempSpan);
+                firstNode = snakeList.shift();
+                firstNode.className = '';
     }
 }
-
