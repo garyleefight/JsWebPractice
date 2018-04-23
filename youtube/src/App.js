ec4633764cee46449bb4bc6018ee655d52b45a13
garyleefight <garyleefight@outlook.com>
2018-04-22 21:23:43 -0700

    add youtube

---
+++ youtube/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './components/search_bar';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+const access_key = 'AIzaSyAQYwgJULiSfQqa5iIdm7hJKArjIVTUhng';
+
+export default App;
