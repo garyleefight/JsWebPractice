f8720ce9a7e81b0ea11305844e4b1c07579369b8
garyleefight <garyleefight@outlook.com>
2018-04-22 21:23:43 -0700

    add youtube

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import SearchBar from './components/search_bar';
 
@@ -12,5 +13,41 @@ class App extends React.Component {
 }
 
 const access_key = 'AIzaSyAQYwgJULiSfQqa5iIdm7hJKArjIVTUhng';
+=======
+import React,{Component} from 'react';
+import SearchBar from './components/search_bar';
+import Youtube from 'youtube-api-search';
+
+const access_key = 'AIzaSyAQYwgJULiSfQqa5iIdm7hJKArjIVTUhng';
+const keyWord = 'Barcelona'
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    this.doSearch(keyWord);
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar videosList={this.state.videos}/>
+      </div>
+    );
+  }
+  doSearch(keyWord) {
+    Youtube({key: access_key, keyWord: keyWord},videos => {
+        console.log(videos);
+        this.setState({
+          videos:videos
+        });
+    });
+  }
+}
+
+
+
+>>>>>>> add youtube
 
 export default App;
--- youtube/src/components/search_bar.js
+++ youtube/src/components/search_bar.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+<<<<<<< HEAD
     render(){
         return(
             <div>
                 abcdefg
+=======
+    
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const {videoList} = this.props;
+        return(
+            <div>
+                {videoList}
+>>>>>>> add youtube
             </div>
         );
     }
